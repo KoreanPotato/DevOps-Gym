d55b766d6ed18769b52ee50048393f32678f6f65
SergeyLim <limsergeystudy@gmail.com>
2025-03-09 22:19:20 +0100

    Second assignment  (Hello, word as a web app)

--- assignment 2/webApp.py
+++ assignment 2/webApp.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def helloFunc():
+    print("Страница открыта")
     return "<h1>Hello, world!!!</h1>"
 
 
 if __name__ == "__main__":
+    print("Запуск сервера...")
     app.run(host="0.0.0.0", port=8080, debug=True)
 
 #Использовал venv для Flask
