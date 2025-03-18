8041ffa8a2080b18e0bf721573fbf3d97b9a9ec4
SergeyLim <limsergeystudy@gmail.com>
2025-03-18 15:42:12 +0100

    fast API

--- assignment_12/index.py
+++ assignment_12/index.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
 import json
 
 app = FastAPI()
 
-@app.get("/api/hello")
-async def getHello(request: Request):
+@app.get("/api/hello", response_class=PlainTextResponse)
+async def get_hello(request: Request):
     body = await request.body()
     
-    if not body:  
-        return {"error": "name is required"}
+    if not body:
+        return "Name is required"
+
+    data = json.loads(body)
+    name = data.get("name")
     
-    data = json.loads(body) 
-    name = data.get("name") 
-    
-    return {f"Hello, {name}!"}
+    return f"Hello, {name}!"
